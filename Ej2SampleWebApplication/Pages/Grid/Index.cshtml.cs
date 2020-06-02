f819f2fc58463474d678719824657b47678df0ec
jz5 <jz5@hotmail.com>
2020-06-02 10:21:17 +0900

    プロジェクト ファイルを追加します。

---
+++ Ej2SampleWebApplication/Pages/Grid/Index.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Ej2SampleWebApplication
+{
+    public class ItemViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime UpdateTime { get; set; }
+        public bool Flag { get; set; }
+    }
+
+    public class IndexModel : PageModel
+    {
+        public List<ItemViewModel> Items = new List<ItemViewModel>();
+
+        public void OnGet()
+        {
+            for (var i = 0; i < 10; i++)
+            {
+                Items.Add(new ItemViewModel
+                {
+                    Id = 100 + i,
+                    Name = $"Item_{i}",
+                    UpdateTime = DateTime.Now.AddDays(-i),
+                });
+            }
+        }
+
+        public IActionResult OnPostUpdate(int id)
+        {
+            var item  = new ItemViewModel
+            {
+                Id = id,
+                UpdateTime = DateTime.Now,
+                Flag = true
+            };
+
+            return new JsonResult(item);
+        }
+    }
+}
